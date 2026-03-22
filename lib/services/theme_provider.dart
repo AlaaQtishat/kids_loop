adc4c9918b9ce4968027ea32610cd7819025d868
AlaaQtishat <alaaqtishat2004@gmail.com>
2026-03-22 22:05:07 +0300

    updated the theme provider, used the shared preferences to save the user's choice.

--- lib/services/theme_provider.dart
+++ lib/services/theme_provider.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool _isDark = false;
+
+  ThemeProvider() {
+    _loadTheme();
+  }
+
   bool get isDark => _isDark;
   ThemeMode get themeMode => _isDark ? ThemeMode.dark : ThemeMode.light;
 
-  void toggleTheme() {
+  void toggleTheme() async {
     _isDark = !_isDark;
+    notifyListeners();
 
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', _isDark);
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDark = prefs.getBool('isDarkMode') ?? false;
     notifyListeners();
   }
 }
