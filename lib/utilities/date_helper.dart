5fa8da23a536ec7b405e2f2aab6ac19ac7fc6216
AlaaQtishat <alaaqtishat2004@gmail.com>
2026-04-01 09:24:40 +0300

    added the localizations to the date helper.

--- lib/utilities/date_helper.dart
+++ lib/utilities/date_helper.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class DateHelper {
   static String getTimeAgo(dynamic createdAtData) {
-    if (createdAtData == null) return "Just now";
+    if (createdAtData == null) return "date_helper.just_now".tr();
 
     DateTime? postDate;
 
@@ -12,12 +13,28 @@ class DateHelper {
       postDate = DateTime.tryParse(createdAtData);
     }
 
-    if (postDate == null) return "Just now";
+    if (postDate == null) return "date_helper.just_now".tr();
 
     final diff = DateTime.now().difference(postDate);
-    if (diff.inDays > 0) return "Posted ${diff.inDays} days ago";
-    if (diff.inHours > 0) return "Posted ${diff.inHours} hrs ago";
-    if (diff.inMinutes > 0) return "Posted ${diff.inMinutes} mins ago";
-    return "Just now";
+
+    if (diff.inDays > 0) {
+      return "date_helper.posted_days_ago".tr(
+        namedArgs: {'days': diff.inDays.toString()},
+      );
+    }
+
+    if (diff.inHours > 0) {
+      return "date_helper.posted_hours_ago".tr(
+        namedArgs: {'hours': diff.inHours.toString()},
+      );
+    }
+
+    if (diff.inMinutes > 0) {
+      return "date_helper.posted_minutes_ago".tr(
+        namedArgs: {'minutes': diff.inMinutes.toString()},
+      );
+    }
+
+    return "date_helper.just_now".tr();
   }
 }
